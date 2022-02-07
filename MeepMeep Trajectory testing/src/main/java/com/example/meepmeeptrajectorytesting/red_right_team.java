d6d6510fb3720a443a32782cce27aa6785ecc17c
flflflf <87872870+flflflf@users.noreply.github.com>
2022-02-07 11:13:17 -0500

    made trajectories

--- MeepMeep Trajectory testing/src/main/java/com/example/meepmeeptrajectorytesting/red_right_team.java
+++ MeepMeep Trajectory testing/src/main/java/com/example/meepmeeptrajectorytesting/red_right_team.java
@@ -1,14 +1,11 @@
 package com.example.meepmeeptrajectorytesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.ColorScheme;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+public class red_right_team {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -17,16 +14,13 @@ public class MeepMeepTesting {
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(90)))
                                 .strafeLeft(10)
-                                .waitSeconds(4)// temperary marker just to visualize the turning
                                 .addDisplacementMarker(() -> {
                                     //spin the thing
                                 })
-                                .strafeTo(new Vector2d(-11,-42))
+                                .lineToSplineHeading(new Pose2d(-24,-37,Math.toRadians(45)))
                                 .addTemporalMarker(()->{
-                                    //put the item to the right height
+                                    //deliver pre loaded block
                                 })
-                                .waitSeconds(4)// also temp
-                                .back(4)
                                 .lineToLinearHeading(new Pose2d(-60,-35,Math.toRadians(0)))
                                 .build()
                         );
---
+++ MeepMeep Trajectory testing/src/main/java/com/example/meepmeeptrajectorytesting/red_solo.java
@@ -0,0 +1,38 @@
+package com.example.meepmeeptrajectorytesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.ColorScheme;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class red_solo {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(52.48180821614297, 52.48180821614297, Math.toRadians(184.02607784577722), Math.toRadians(184.02607784577722), 16.34)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(90)))
+                                .strafeLeft(10)
+                                .addDisplacementMarker(() -> {
+                                    //spin the thing
+                                })
+                                .strafeTo(new Vector2d(-11,-42))
+                                .addTemporalMarker(()->{
+                                    //put the item to the right height
+                                })
+                                .back(4)
+                                .lineToLinearHeading(new Pose2d(-60,-35,Math.toRadians(0)))
+                                .build()
+                        );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
