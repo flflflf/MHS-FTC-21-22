db1820025e4bab92f7752e4c1a96eabccdb58066
Felix <felixjgrimm@gmail.com>
2022-02-08 14:38:24 -0500

    fixed the null pointer exception
    fixed the null pointer exception with hardwareMap in the AutoTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ours/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ours/AutoTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Ours.Vision;
+package org.firstinspires.ftc.teamcode.Ours;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -8,15 +8,17 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous(name = "trajectoryTest")
 public class AutoTest extends LinearOpMode {
-    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
     Pose2d startPos = new Pose2d(-35, -62, Math.toRadians(90));
     @Override
     public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPos)
                 .forward(2)
                 .strafeLeft(10)
                 .lineToSplineHeading(new Pose2d(-24, -37, Math.toRadians(45)))
                 .lineToLinearHeading(new Pose2d(-60, -35, Math.toRadians(0)))
                 .build();
+        waitForStart();
+        drive.followTrajectorySequence(trajSeq);
     }
 }
